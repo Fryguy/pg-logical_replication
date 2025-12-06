51f72aaccb6633930b32e58f1d4ef47837aac379
Jason Frey <fryguy9@gmail.com>
2025-12-05 20:14:01 -0500

    Fix database connection using a socket on CI

--- spec/support/connection_helper.rb
+++ spec/support/connection_helper.rb
@@ -1,12 +1,25 @@
 module ConnectionHelper
+  def self.connection_for(dbname)
+    require "pg"
+
+    options = {
+      :host     => ENV["POSTGRESQL_HOST"],
+      :user     => ENV["POSTGRESQL_USER"],
+      :password => ENV["POSTGRESQL_PASSWORD"],
+      :dbname   => dbname
+    }.compact
+
+    PG::Connection.new(options)
+  end
+
   def self.source_database_connection
-    @source_database_connection ||= PG::Connection.new(:dbname => "logical_test").tap do |c|
+    @source_database_connection ||= connection_for("logical_test").tap do |c|
       c.set_notice_receiver { |r| nil }
     end
   end
 
   def self.target_database_connection
-    @target_database_connection ||= PG::Connection.new(:dbname => "logical_test_target").tap do |c|
+    @target_database_connection ||= connection_for("logical_test_target").tap do |c|
       c.set_notice_receiver { |r| nil }
     end
   end
